b89706976ad70a52a5d930bdad117de17adf6aef
PhinesNguyen <nguyenquangtien2005z@gmail.com>
2026-01-23 18:00:42 +0700

    update

--- Mobilefront/App.tsx
+++ Mobilefront/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import RootNavigator from './src/navigation/RootNavigator';
+import BottomTabNavigator from './frontend/src/navigation/BottomTabNavigator';
+import RootNavigator from './frontend/src/navigation/RootNavigator';
 export default function App() {
   return (
     <SafeAreaProvider>
