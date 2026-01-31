1634f583fc6e2a76d78f337a00977d4b5251401d
Duy <himiko.hikari@gmail.com>
2026-01-31 23:01:25 +0700

    fix ai api

---
+++ Webfront/src/api/aiAxiosClient.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const aiAxiosClient = axios.create({
+  baseURL: "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 30000,
+});
+
+aiAxiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("AI Service Error:", {
+      status: error.response?.status,
+      data: error.response?.data,
+      message: error.message,
+    });
+    return Promise.reject(error);
+  },
+);
+
+export default aiAxiosClient;
